60f4e9e308302c19b9800547cf278a6eb7829771
KimChangWoo <so040602@gmail.com>
2024-08-05 12:38:01 +0900

    7번째 작업

--- src/main/java/com/itgroup/jdbc/DeleteMain.java
+++ src/main/java/com/itgroup/jdbc/DeleteMain.java
@@ -8,7 +8,7 @@ public class DeleteMain {
     public static void main(String[] args) {
         // 날씨가 많이 덥네요.
         // 오늘 점심은 시원한 것으로 먹읍시다.
-        // 내가 먼저 푸시함
+        // 다시 내가 먼저 푸시함
 
         Scanner scan = new Scanner(System.in);
         GamaDao dao = new GamaDao();
